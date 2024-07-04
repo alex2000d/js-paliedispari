90227cb8a9de645f2de0f74aa0c95ed10f230473
alex2000d <demarcoalessandro777@gmail.com>
2024-07-04 21:13:31 +0200

    [UPD] exercise completed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@ let word = prompt('inserisci la tua parola');
 let check_pal = word.split('').reverse().join('');
 // creo una funzione
 function countWords (string){
-   
+    // creo un ciclo for
+    for( let i = 0; i < string.length; i++){
+        // se e uguale allora è palindroma
+                if(word == check_pal){
+                    console.log('la stringa e palindroma')
+                    // altrimenti non è palindroma
+                }else {
+                    console.log('la stringa non e palindroma')
+                    return'la stringa non e palindroma'
+               
+                } 
+            }
+            // mostro in console il risultato
+            return('la stringa non e palindroma')
     }
     // chiamo la funzione
     let wordpal = countWords(word);
