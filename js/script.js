28b8f01bd9d5ab4c2fa7ea6411f0477904806b23
alex2000d <demarcoalessandro777@gmail.com>
2024-07-04 21:12:51 +0200

    [UPD] created the function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@
 let word = prompt('inserisci la tua parola');
 // divido la parola
 let check_pal = word.split('').reverse().join('');
+// creo una funzione
+function countWords (string){
+   
+    }
+    // chiamo la funzione
+    let wordpal = countWords(word);
