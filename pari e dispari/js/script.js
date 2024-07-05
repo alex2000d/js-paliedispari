08120450ae2d9c869341f683ea4204fb26a20226
alex2000d <demarcoalessandro777@gmail.com>
2024-07-05 16:48:00 +0200

    [UPD] exercise complete

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -20,3 +20,19 @@ let even_or_odd = prompt('insersci pari o dispari');
 // creo un promt per inserire un numero da uno a cinque
 let number = parseInt(prompt('inserisci un numero da 1 a 5'));
 
+// genero il numero per il pc richiamando la funzione generate random number
+let pc_number = generaterandomnumber();
+console.log(pc_number);
+// faccio la somma dei due numeri
+let sum = number + pc_number;
+console.log(sum);
+// dichiaro se il risultato è pari o dispari
+let result = checkevenorodd(sum);
+
+// verifico che il risultato e la variabile pari o dispari hanno lo stesso valore e gli assegno se ha vinto o perso
+if(even_or_odd === result){
+    console.log('Hai vinto');
+}
+else{
+    console.log('Hai perso');
+}
